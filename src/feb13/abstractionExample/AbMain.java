f867a05350baa7c91891eb8df4c00946aed77cd4
13tonight <gurramhaswanth1999@gmail.com>
2024-02-13 20:06:20 -0600

    abstraction

---
+++ src/feb13/abstractionExample/AbMain.java
@@ -0,0 +1,10 @@
+package feb13.abstractionExample;
+
+public class AbMain {
+    public static void main(String[] args) {
+        AIphone  newAi = new AIphone();
+        newAi.call();
+        newAi.mobile();
+        newAi.cordless();
+    }
+}
---
+++ src/feb13/abstractionExample/phone.java
@@ -0,0 +1,33 @@
+package feb13.abstractionExample;
+
+public abstract class phone {
+
+    public  abstract void call();
+
+
+}
+abstract class mobile extends phone{
+
+    public abstract void cordless();
+}
+abstract class smartphone extends mobile{
+    public abstract void mobile();
+}
+ class AIphone extends smartphone{
+
+
+     @Override
+     public void call() {
+         System.out.println("calling");
+     }
+
+     @Override
+     public void cordless() {
+         System.out.println("using a cordless phone");
+     }
+
+     @Override
+     public void mobile() {
+         System.out.println("mobile phone");
+     }
+ }
